b0eeae65135a8fa790c4ed840a9c9ac9fa8a6fd3
Vishvananda Ishaya Abrams <vish.ishaya@oracle.com>
2017-07-03 17:35:52 -0700

    Update musl build and wercker config

--- build_seccomp.sh
+++ build_seccomp.sh
@@ -10,6 +10,6 @@ function die {
 [ -e libseccomp/autogen.sh ] || die "libseccomp empty, did you git submodule update --init"
 cd libseccomp
 [ -e ./configure ] || ./autogen.sh || die "autogen failed, check dependencies"
-[ -e ./Makefile ] || CC=musl-gcc CPPFLAGS="-idirafter/usr/include/x86_64-linux-gnu -idirafter/usr/include" ./configure --enable-static
+[ -e ./Makefile ] || CC="musl-gcc -pie -fPIC" CPPFLAGS="-idirafter/usr/include/x86_64-linux-gnu -idirafter/usr/include" ./configure --enable-static
 make || die "make failed, check dependencies"
 echo "$PWD/src/.libs"
